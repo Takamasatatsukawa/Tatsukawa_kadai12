2c8cc5861605ef8ca291f327754e93c2b8bda2c5
tatsukawa <tatsukawatakamasa@gmail.com>
2024-06-25 12:45:26 +0900

    修正４

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -15,7 +15,7 @@ class ExercisesController < ApplicationController
     # 【要件】配達先の一番多い住所を返すこと
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
-    @address = Address.joins (:orders)
+    @address = Address.joins(:orders)
                       .select('addresses.*, COUNT(orders.id) as orders_count')
                       .group('addresses.id')
                       .order('orders_count DESC')
